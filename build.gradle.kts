e661aed4002e235be84bbc90321af153198eb02f
Tomasz Linkowski <t.linkowski@gmail.com>
2019-03-10 15:16:01 +0100

    #2: Init gradle configuration

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+tasks.wrapper {
+    gradleVersion = "5.2.1"
+    distributionType = Wrapper.DistributionType.ALL
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    group = "pl.tlinkowski"
+}
+
+apply(from = "$rootDir/gradle/ide.gradle.kts")
---
+++ settings.gradle.kts
@@ -0,0 +1,44 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2019 Tomasz Linkowski.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+rootProject.name = "basic-annotations"
+
+/*
+ * Source: https://aalmiray.github.io/kordamp-gradle-plugins/#_project_structure
+ */
+fun includeProject(projectDirName: String, projectName: String) {
+    val baseDir = File(settingsDir, projectDirName)
+    val projectDir = File(baseDir, projectName)
+    val buildFileName = "$projectName.gradle.kts"
+
+    assert(projectDir.isDirectory())
+    assert(File(projectDir, buildFileName).isFile())
+
+    include(projectName)
+    project(":$projectName").projectDir = projectDir
+    project(":$projectName").buildFileName = buildFileName
+}
+
+listOf("subprojects").forEach { dirName ->
+    val subdir = File(rootDir, dirName)
+    subdir.walkTopDown().forEach { dir ->
+        val buildFile = File(dir, "${dir.name}.gradle.kts")
+        if (buildFile.exists()) {
+            includeProject(dirName, dir.name)
+        }
+    }
+}
