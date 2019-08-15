b58a16f4415cbe6839f75f7c119e5f08924e04d6
Tomasz Linkowski <t.linkowski@gmail.com>
2019-08-15 07:24:41 +0200

    Add documentation to module info

--- subprojects/basic-annotations/src/main/java/module-info.java
+++ subprojects/basic-annotations/src/main/java/module-info.java
@@ -15,7 +15,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /**
+ * Provides a set of basic annotations based on <a href="https://jcp.org/en/jsr/detail?id=305">JSR 305</a> and
+ * <a href="https://github.com/JetBrains/kotlin/tree/master/libraries/tools/kotlin-annotations-jvm">Kotlin JVM
+ * annotations</a>.
+ * <p>
+ * The following annotations are to be used by the consumers of this library:
+ * <ul>
+ *   <li>for nullability: {@link pl.tlinkowski.annotation.basic.NonNullPackage}, {@link pl.tlinkowski.annotation.basic.NullOr}</li>
+ *   <li>for collection mutability: {@code kotlin.annotations.jvm.ReadOnly}, {@code kotlin.annotations.jvm.Mutable}</li>
+ *   <li>miscellaneous: {@link pl.tlinkowski.annotation.basic.VisibleForTesting}</li>
+ * </ul>
+ * <p>
+ * More details can be found in the <a href="https://github.com/tlinkowski/basic-annotations">GitHub README</a>.
+ * <p>
+ * Note: even though this library provides a {@code module-info.class}, all its other classes are compatible with JDK 8.
+ *
  * @author Tomasz Linkowski
  */
 module pl.tlinkowski.annotation.basic {
